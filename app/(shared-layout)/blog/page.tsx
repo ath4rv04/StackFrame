28aeff1976d21d16c2774a07cbad7c1f5f541f49
atharv <atharv.shawrikar.7@gmail.com>
2026-02-10 11:30:01 +0530

    added streaming component in the blog page

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -1,22 +1,37 @@
-"use client"
-
 import { useQuery } from "convex/react"; //client side component
 import { api } from "@/convex/_generated/api";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import { fetchQuery } from "convex/nextjs";
+import { resolve } from "path";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function blog() {
-    const data = useQuery(api.posts.getPost)
+    
     return(
         <div className="py-12">
             <div className="text-center pb-12">
                 <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl">Our Blog</h1>
                 <p className="pt-4 max-w-2xl mx-auto text-xl text-muted-foreground">Insights, thoughts, and trends from our team.</p>
             </div>
+            <Suspense fallback={
+                <SkeletonLoading/>
+            }>
+                <LoadBlog />
+            </Suspense>
+        </div>
+    ); // data is being fetched on the client side thats why we dont see it in the first time
+}
 
-            <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+async function LoadBlog() {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    const data = await fetchQuery(api.posts.getPost);
+
+    return (
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
                 {data?.map((posts) => (
                     <Card key = {posts._id} className="pt-0">
                         <div className="relative h-48 w-full overflow-hidden">
@@ -44,8 +59,25 @@ export default function blog() {
                     </Card>
                 ))}
             </div>
-        </div>
-    ) // data is being fetched on the client side thats why we dont see it in the first time
+    )
+}
+
+function SkeletonLoading() {
+    return (
+        <div className="grid gap-6 md: grid-cols-3 lg:grid-cols-3">
+                    {[...Array(3)].map((_, i) => (
+                        <div key={i} className="flex flex-col space-y-3">
+                            <Skeleton className="h-48 w-full rounded-xl"/>
+                            <div className="space-y-2 flex flex-col">
+                                <Skeleton className="h-6 w-3/4" />
+                                <Skeleton className="h-4 w-full" />
+                                <Skeleton className="h-4 w-2/3" />
+                            </div>
+                        </div>
+                    ))
+                    }
+                </div>
+    )
 }
 
 
@@ -54,3 +86,15 @@ export default function blog() {
 //when you make a req the client component is pre render on the server side.
 //After the html is fully loaded it downloads the js and executes it.
  
+//new learning
+
+//client makes a request, Nextjs server makes the req to convex (baas), 
+//convex executes the correct function and return back to 
+//next js and generates the hmtl which is sent to the client
+
+//we use fetchQuery to make it a server component. There is no hydration or js
+//when we use fetchQuery() there is no client involved anymore so there is no way to make connection. Its a limitation
+//You can fetch data on the server side but you dont get any updates and the reactive benefits of convex
+//the blog page doesnt need any reactivity tho
+
+//convex provide preloading to retain reactivity use preloadQuery()
